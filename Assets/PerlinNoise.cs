d8ccdec8bb15958c686e7a478965d55ec8f19717
fran516 <rzq512@york.ac.uk>
2025-11-19 15:00:18 +0000

    19/11/25

--- Assets/PerlinNoise.cs
+++ Assets/PerlinNoise.cs
@@ -2,16 +2,19 @@ using UnityEngine;
 
 public class PerlinNoise : MonoBehaviour
 {
-    public int Intensity;
+    public float minIntensity = 0.5f;
+    public float maxIntensity = 2.0f;
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        GetComponent<Light>().intensity = Mathf.PerlinNoise(Random.Range(minIntensity, maxIntensity),1f);
     }
 }

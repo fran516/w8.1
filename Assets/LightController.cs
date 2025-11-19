0cfeefa881809af378b309bb8464e7930b869593
fran516 <rzq512@york.ac.uk>
2025-11-19 10:47:52 +0000

    19/12/25

--- Assets/LightController.cs
+++ Assets/LightController.cs
@@ -2,6 +2,9 @@ using UnityEngine;
 
 public class LightController : MonoBehaviour
 {
+    public bool status = false;
+    public GameObject light;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +14,13 @@ public class LightController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.F) && status == false) {
+            light.SetActive(true);
+            status = true;
+        } else if(Input.GetKeyDown(KeyCode.F) && status == true) {
+            light.SetActive(false);
+            status = false;
+        }
+ 
     }
 }
